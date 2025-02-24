40773926b88d35a344f3f56779b21f26a6fe24b3
k1105 <kanapon1105@yahoo.co.jp>
2025-02-25 02:34:42 +0900

    remove metadata

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,18 +1,18 @@
-import type { Metadata } from "next";
+import Head from "next/head";
 import "./globals.css";
-
-export const metadata: Metadata = {
-  title: "DLT Meishi Generator",
-  description: "by Kanata Yamagishi",
-};
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Meishi Generator</title>
+      </Head>
       <body>{children}</body>
     </html>
   );
